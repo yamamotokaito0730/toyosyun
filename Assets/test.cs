0ec7b6a66e5e26eaecbe43da8d4503d577c0a5d6
yamamotokaito0730 <149446520+yamamotokaito0730@users.noreply.github.com>
2025-04-07 11:55:17 +0900

    test

--- Assets/test.cs
+++ Assets/test.cs
@@ -5,7 +5,7 @@ public class test : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        int a = 10; 
     }
 
     // Update is called once per frame
